61a424fcd0c465e0ca5fcb673956856f42f5b919
wnadurski <wnadurski@gmail.com>
2015-12-13 21:56:48 +0100

    wrapper

--- static/app/filters/milestokm.js
+++ static/app/filters/milestokm.js
@@ -1,9 +1,12 @@
 /**
  * Created by hyster on 13.12.15.
  */
-angular.module("evs").
-    filter('milestokm', function() {
-    return function(input) {
-        return Math.round(input*1.609344);
-    }
-});
+(function()
+{
+    angular.module("evs").
+    filter('milestokm', function () {
+        return function (input) {
+            return Math.round(input * 1.609344);
+        }
+    });
+})();
